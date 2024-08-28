5248a0f04768db23f47a5871a1e9c746b5d34a84
Mo <117493882+ademoyosoreoluwa@users.noreply.github.com>
2024-08-28 21:14:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,32 @@ const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // Prompt to select Media Stream, then play
-
-async function selectMediaStream(){
-    try{
+async function selectMediaStream() {
+    try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
         videoElement.srcObject = mediaStream;
         videoElement.onloadedmetadata = () => {
-            videoElement.onplay();
+            videoElement.play(); // Corrected play method
         }
     } catch (error) {
-        console.log('Whops, error here:', error);
-
+        console.log('Whoops, error here:', error);
     }
 }
 
-button.addEventListener('click', async() => {
+button.addEventListener('click', async () => {
     // Disable Button
     button.disabled = true;
-    //  Start Picture in Picture 
-    await videoElement.requestPictureInPicture();
+    
+    // Start Picture in Picture
+    try {
+        await videoElement.requestPictureInPicture();
+    } catch (error) {
+        console.log('Failed to start Picture-in-Picture:', error);
+    }
+
     // Reset Button
     button.disabled = false;
-})
-
-// 
+});
 
 // On Load
 selectMediaStream();
